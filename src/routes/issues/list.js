58257c2d32a4caaac8b94a21717768fcf300c6b3
Andreas Nedbal <andy@pixelde.su>
2020-11-14 02:21:31 +0100

    :recycle: only use required methods

--- src/routes/issues/list.js
+++ src/routes/issues/list.js
@@ -1,7 +1,7 @@
-const fs = require('fs')
+const { readFile } = require('fs')
 const { join } = require('path')
-const util = require('util')
-const readFile = util.promisify(fs.readFile)
+const { promisify } = require('util')
+const readFileP = promisify(readFile)
 
 module.exports = ['/issues.:format', async (req, res) => {
   let file = null;
@@ -9,11 +9,11 @@ module.exports = ['/issues.:format', async (req, res) => {
   switch(req.params.format) {
     case 'json':
       res.type('json')
-      file = await readFile(join(__dirname, 'data/list.json'))
+      file = await readFileP(join(__dirname, 'data/list.json'))
       break
     case 'xml':
       res.type('xml')
-      file = await readFile(join(__dirname, 'data/list.xml'))
+      file = await readFileP(join(__dirname, 'data/list.xml'))
       break
     default:
       res.status(406).send()
